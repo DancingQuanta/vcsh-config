d1c10910a481d0b1b399e453e1173250d34caaf4
DancingQuanta <andytheseeker@gmail.com>
2017-11-07 08:55:57 +0000

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+## DancingQuanta/vcsh-config - https://github.com/DancingQuanta/vcsh-config 
+## bootstrap.sh
+## bootstrap.sh bootstraps the root vcsh by setting up useful hooks for vcsh 
+## and configuring mr to be extendable.
+##
+## Needed software:
+## - git
+## - perl
+## - mr
+## - vcsh
+##
+## It is inspired by the bootstrap script by Vincend Demeester at
+## https://github.com/vdemeester/vcsh-home/blob/bootstrap/bootstrap.sh
+## and https://github.com/ek9/vcsh-dotfiles/blob/master/vcsh-dotfiles
+## and https://gist.github.com/elerch/88ea951c9c4ec4c3c1604b8fc9167e53
+
+# To run this without copy/paste the whole thing:
+# curl > bootstrap.sh && sh bootstrap.sh && rm bootstrap.sh
+
+## ---------------------------------------------------------------------------
+## Variables
+## ---------------------------------------------------------------------------
+SELF="$(basename $0)"
+SELF_URL="https://github.com/DancingQuanta/vcsh-config"
+
+## ---------------------------------------------------------------------------
+## Functions
+## ---------------------------------------------------------------------------
+# *log*: a wrapper of echo to print stuff in a more colorful way
+log() {
+  ECHO_ARGS=""
+  test "$1" = "-n" && {
+    ECHO_ARGS="-n"
+    shift
+  }
+  echo $ECHO_ARGS "$(tput sgr0)$(tput setaf 2)>$(tput bold)>$(tput sgr0) $*"
+}
+# *warn*: a wrapper of echo to print stuff in a more colorful way, warning
+warn() {
+  test "$1" = "-n" && {
+    ECHO_ARGS="-n"
+    shift
+  }
+  echo $ECHO_ARGS "$(tput sgr0)$(tput setaf 3)<$(tput bold)<$(tput sgr0) $*"
+}
+# *fatal*: a wrapper of echo to print stuff in a more colorful way, error
+fatal () {
+  test "$1" = "-n" && {
+    ECHO_ARGS="-n"
+    shift
+  }
+  echo $ECHO_ARGS "$(tput sgr0)$(tput setaf 9)<$(tput bold)<$(tput sgr0) $*" >&2
+  exit $2
+}
+# *check_cmd* : check a command and fail if not present
+check_cmd() {
+    command -v $1 >/dev/null && {
+        echo "   $1"
+    } || {
+        echo ""
+        warn "$1 is not available"
+        echo
+        exit 1
+    }
+}
+
+## ---------------------------------------------------------------------------
+## Bootstrap
+## ---------------------------------------------------------------------------
+
+log "Checking required software"
+check_cmd perl
+check_cmd git
+check_cmd vcsh
+check_cmd mr
+
+log "Cloning $VCSH_ROOT"
+cd $HOME
+# Clone the root vcsh repo, containing the mr configuration
+[ ! -d ~/.config/vcsh/repo.d/$VCSH_REPO.git ] && vcsh clone $VCSH_ROOT
+
+# Fixup $VCSH_REPO's working tree for the sparse checkout settings
+log "Setting up $VCSH_REPO"
+vcsh $VCSH_REPO read-tree -mu HEAD
+mr update
